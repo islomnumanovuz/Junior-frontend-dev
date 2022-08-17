f768ecefdfc37fc4aa1b2c3b3326e602031271c0
islomnumanovuz <islomnumanov.uz@gmail.com>
2022-08-17 16:48:10 +0300

    feat: Add js statement, variables, casesensative

---
+++ Модуль 1. База JavaScript/2. Синтаксис/script.js
@@ -0,0 +1,24 @@
+console.log("Hello World!");
+// ; is optional
+
+// Two types of comment: oneline and multiline
+// This is oneline comment
+/*
+ This is multiline comment it ignores
+*/
+
+// Variables - переменная - var/let/const
+
+var text = "Hello world";
+let name = "Maksim";
+const programmingLanguage = "JavaScript";
+console.log(text);
+console.log(name);
+console.log(programmingLanguage);
+text = "Hello";
+console.log(text);
+name = "Mamat";
+// programmingLanguage = "Python" = TypeError const
+
+// JavaScript - caseSensative
+// console.log(Name) - Reference Error name is not defined

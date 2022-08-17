5bd4098b40b8d0887ac4cda1ca9478f0fbba45e7
islomnumanovuz <islomnumanov.uz@gmail.com>
2022-08-17 16:22:40 +0300

    feat: Add all hometask

---
+++ Модуль 1. База JavaScript/1. Hello World/hometask/task_2/first-project/index.js
@@ -0,0 +1,9 @@
+console.log("Запускаем таймер")
+alert(6);
+alert(5);
+alert(4);
+alert(3);
+alert(2);
+alert(1);
+alert(0);
+console.log("Обратный отчет закончен")
---
+++ Модуль 1. База JavaScript/1. Hello World/hometask/task_3/first-project/js-info.js
@@ -0,0 +1,4 @@
+alert("Кто разработал язык программирования JavaScript?");
+alert("В каком году появился JavaScript?")
+alert("JavaScript- это то же самое, что и Java?")
+alert("Как называется спецификация JavaScript?")

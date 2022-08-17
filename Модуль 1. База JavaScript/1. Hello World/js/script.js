17487eadee766a73d08b05818f1721aab7b91169
islomnumanovuz <islomnumanov.uz@gmail.com>
2022-08-17 03:17:54 +0300

    feat: Add first js code

---
+++ Модуль 1. База JavaScript/1. Hello World/js/script.js
@@ -0,0 +1,10 @@
+// window.console.log("Hello world!");
+// 'string' - строка
+console.log("Hello world! 1");
+console.log("Hello world! 2");
+console.log("Hello world! 3");
+
+window.alert("This is alert box");
+
+// window.prompt("Message", default)
+prompt("This is javascript promt box", "Hello");
